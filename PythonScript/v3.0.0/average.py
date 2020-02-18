ea5088a5f7b1eb091ce6be2090ddd566073d86bd
Nil Marti Hermans <nil.martihermans@student.ucll.be>
2020-02-18 10:49:25 +0100

    Name changes

---
+++ PythonScript/v3.0.0/average.py
@@ -0,0 +1,14 @@
+def average():
+
+  # Get three test score
+  round1 = 30000
+
+  round2 = 15000
+
+  round3 = 10000
+
+  # Calculate the average
+  average = (round1 + round2 + round3) / 3
+
+  # Print out the test score
+  print (average)
